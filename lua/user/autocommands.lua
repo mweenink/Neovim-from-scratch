a76513ed582a2ef7b5d202317a0af279f5c3ca65
M. Weenink <git@waas.cloud>
2022-08-04 13:31:20 +0200

    Mostly changed original to add templates

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,6 +28,17 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  augroup templates
+    autocmd BufNewFile *.html 0r ~/.vim/skeleton/bootstrap.py
+    autocmd BufNewFile *.py 0r ~/.vim/skeleton/python3.py
+    autocmd BufNewFile *.c 0r ~/.vim/skeleton/main.c
+  augroup END
+
+  augroup buildsystems
+    autocmd!
+      autocmd FileType c setlocal makeprg=gcc\ -Wall\ -g\ %\ %<
+      autocmd FileType python setlocal makeprg=python3\ %
 ]]
 
 -- Autoformat
